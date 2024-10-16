2609975fe65d1e4aed1830dab563812a42766dd0
jwagner8 <131898690+jwagner8@users.noreply.github.com>
2024-10-16 19:31:30 -0400

    Update HR.kt

--- src/main/kotlin/edu/trincoll/hr/HR.kt
+++ src/main/kotlin/edu/trincoll/hr/HR.kt
@@ -1,10 +1,33 @@
 package edu.trincoll.hr
 
-// The HR class should have:
-//   - a list of employees
-//   - a hire method that takes an employee and returns a new HR object with that employee added
-//   - a fire method that takes an id and returns a new HR object with the employee with that id removed
-//   - a payEmployees method that returns the total pay of all employees
-class HR(private val employees: List<Employee> = emptyList()) {
+// Hourly class should extend the Employee class. That means
+// its constructor should take a name of type String and an id of type Int,
+// and pass them to the Employee constructor.
+//
+// It also should have:
+//   - a "rate" field of type Double
+//   - a "hours" field of type Double, with a default value of 80.0
+//
+// It should override the pay() method to return the rate times the hours.
+//
+// It should override the toString method to return a string with the
+// name, id, rate, and hours of the employee.
 
+// Hourly class extending Employee
+class Hourly(
+    name: String,
+    id: Int,
+    val rate: Double,
+    val hours: Double = 80.0  // Default value for hours is 80.0
+) : Employee(name, id) {
+
+    // Override the pay method to return rate * hours
+    override fun pay(): Double {
+        return rate * hours
+    }
+
+    // Override toString to return detailed information
+    override fun toString(): String {
+        return "Hourly(name=$name, id=$id, rate=$rate, hours=$hours)"
+    }
 }
